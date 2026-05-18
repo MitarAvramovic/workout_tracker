65b31c3a2fc5a1fdb4cdc207ab91d1f3008e230a
MitarAvramovic <mitar10.avramovic@gmail.com>
2026-05-18 13:42:57 +0200

    fix 01

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-set -e
 
-python -m pip install -r requirements.txt
+pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py create_superuser_if_none
