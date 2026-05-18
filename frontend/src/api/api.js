c74221a726cff31620851205f8e1e32bb5eb3de1
MitarAvramovic <mitar10.avramovic@gmail.com>
2026-05-18 22:22:04 +0200

    Uspelo

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -5,27 +5,6 @@ export const api = axios.create({
     withCredentials: true,
 });
 
-// robust cookie reader
-function getCookie(name) {
-    let cookieValue = null;
-
-    if (document.cookie && document.cookie !== "") {
-        const cookies = document.cookie.split(";");
-
-        for (let cookie of cookies) {
-            cookie = cookie.trim();
-
-            if (cookie.startsWith(name + "=")) {
-                cookieValue = decodeURIComponent(
-                    cookie.substring(name.length + 1)
-                );
-                break;
-            }
-        }
-    }
-
-    return cookieValue;
-}
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("authToken");
